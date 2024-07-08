e4beac0b36df1fd76c8b4d41090f36d5fa46e600
GVitSer <guvitsvst@mail.ru>
2024-07-08 03:30:14 +0300

    Dz4

---
+++ src/components/footer/component.jsx
@@ -0,0 +1,23 @@
+export const Footer = ()=>{
+  return (
+<footer>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>  
+<br/>
+<br/>
+<br/>
+GVS
+</footer>
+  );
+};
